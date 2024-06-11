6d3f0dcdbd25c59a4fec09a4aa5492f857ee9381
Hazem ali <81050076+hazemali16@users.noreply.github.com>
2024-06-11 23:15:15 +0300

    Add files via upload

---
+++ app/components/Services.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import service1 from "../images/DSC07815.jpg"
+import service2 from "../images/DSC07856.jpg"
+import service3 from "../images/DSC07822.jpg"
+
+
+const Services = () => {
+  return (
+    <div className="w-10/12 mx-auto py-40 text-center opacity-0 translate-y-10 duration-500" id="services">
+      <h2 className="mb-8 text-3xl font-bold text-[var(--main-color)]">خدماتنا</h2>
+      <p className="w-1/2 leading-relaxed mx-auto max-lg:w-3/4 max-md:w-full">يفهم وايت قاردن مدى توقعاتكم ومعايير الجمال التي تليق بكل اللحظات واللقاءات والمناسبات؛ ليقدم لكم تشكيلة مأكولات وقهوة صنعت بحب وتجدد عن كل لقاء لتكتشفوا أروع صور التميز والإبداع في فنون الطهي وعالم النكهات التي تناسب كل الأعمار.</p>
+      <div className="grid mt-20 grid-cols-3 gap-5 max-lg:grid-cols-2 max-md:grid-cols-1">
+        <div className="service opacity-0 translate-y-10 duration-500 delay-500">
+          <Image src={service1} alt="service1"/>
+        </div>        
+        <div className="service opacity-0 translate-y-10 duration-500 delay-700">
+          <Image src={service2} alt="service2"/>
+        </div>        
+        <div className="service opacity-0 translate-y-10 duration-500 delay-1000">
+          <Image src={service3} alt="service3"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
---
+++ app/page.js
@@ -0,0 +1,80 @@
+'use client'
+import Services from "./components/Services";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import { useEffect } from "react";
+import Menu from "./components/Menu";
+
+
+export default function Home() {
+  useEffect(() => {
+    let services = document.querySelector("#services")
+    let menu = document.querySelector("#menu")
+    // let gallery = document.querySelector(".gallery")
+    // let hopes = document.querySelector(".hopes")
+
+    window.onscroll = () => {
+      if (window.scrollY >= services.offsetTop - 600) {
+        services.classList.remove("opacity-0")
+        services.classList.remove("translate-y-10")
+        for (let index = 0; index < services.children[2].children.length; index++) {
+          services.children[2].children[index].classList.remove("opacity-0")
+          services.children[2].children[index].classList.remove("translate-y-10")
+        }
+      }
+      if (window.scrollY >= menu.offsetTop - 600) {
+        menu.classList.remove("opacity-0")
+        menu.classList.remove("translate-y-10")
+      }
+      // if (window.scrollY >= gallery.offsetTop - 600) {
+      //   gallery.classList.remove("opacity-0")
+      //   gallery.classList.remove("translate-y-10")
+      //   for (let index = 0; index < gallery.children[2].children.length; index++) {
+      //     gallery.children[2].children[index].classList.remove("opacity-0")
+      //     gallery.children[2].children[index].classList.remove("translate-y-10")
+      //   }
+      // }
+      // if (window.scrollY >= hopes.offsetTop - 600) {
+      //   hopes.classList.remove("opacity-0")
+      //   hopes.classList.remove("translate-y-10")
+      // }
+    }
+    let isDragStart = false, prevPageX, PrevScrollLeft
+    let leftBtn = document.querySelector(".left-btn")
+    let rightBtn = document.querySelector(".right-btn")
+    let slider = document.querySelector(".slider")
+    let firstImageWidth = slider.children[0].children[0].children[0].clientWidth
+    rightBtn.onclick = () => {
+        slider.scrollLeft += firstImageWidth + (slider.children[0].clientWidth * .1)
+    }
+    leftBtn.onclick = () => {
+      slider.scrollLeft -= firstImageWidth + (slider.children[0].clientWidth * .1)
+    }
+    slider.addEventListener("mousemove", (e) => {
+      if (!isDragStart) return;
+      e.preventDefault();
+      slider.scrollLeft = PrevScrollLeft - e.pageX + prevPageX
+    })
+
+    slider.addEventListener("mousedown", (e) => {
+      isDragStart = true
+      prevPageX = e.pageX
+      PrevScrollLeft = slider.scrollLeft
+      slider.classList.add("slider-grabbing")
+    })
+
+    slider.addEventListener("mouseup", (e) => {
+      isDragStart = false
+      slider.classList.remove("slider-grabbing")
+    })
+  
+}, [])
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Services />
+      <Menu />
+    </div>
+  );
+}
