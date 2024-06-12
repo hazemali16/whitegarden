36333ed920393d95c7ada55b8cf314cc931203db
Hazem ali <81050076+hazemali16@users.noreply.github.com>
2024-06-12 22:35:32 +0300

    Update Services.jsx

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -14,7 +14,7 @@ import service9 from "../images/services6.jpg"
 const Services = () => {
   return (
     <div className="w-10/12 mx-auto py-40 text-center opacity-0 translate-y-10 duration-500" id="services">
-      <p className="w-1/2 leading-relaxed mx-auto max-lg:w-3/4 max-md:w-full text-xl">يفهم وايت قاردن مدى توقعاتكم ومعايير الجمال التي تليق بكل اللحظات واللقاءات والمناسبات؛ ليقدم لكم تشكيلة مأكولات وقهوة صنعت بحب وتجدد عن كل لقاء لتكتشفوا أروع صور التميز والإبداع في فنون الطهي وعالم النكهات التي تناسب كل الأعمار.</p>
+      <p className="w-1/2 leading-relaxed mx-auto max-lg:w-3/4 max-md:w-full text-xl text-[var(--heading-color)]">يفهم وايت قاردن مدى توقعاتكم ومعايير الجمال التي تليق بكل اللحظات واللقاءات والمناسبات؛ ليقدم لكم تشكيلة مأكولات وقهوة صنعت بحب وتجدد عن كل لقاء لتكتشفوا أروع صور التميز والإبداع في فنون الطهي وعالم النكهات التي تناسب كل الأعمار.</p>
       <div className="grid mt-20 grid-cols-3 gap-5 max-lg:grid-cols-2 max-md:grid-cols-1">
         <div className="service opacity-0 translate-y-10 duration-500 delay-500">
           <Image src={service1} alt="service1"/>
