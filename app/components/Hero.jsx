794381feafb348f02d928f5fa32208f6fac566b3
Hazem ali <81050076+hazemali16@users.noreply.github.com>
2024-06-13 12:20:09 +0300

    Update Hero.jsx

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="absolute w-full h-full top-0 left-0">
       <Image className="w-full h-full object-cover" src={hero} alt="hero" />
     </div>
-    <div className="relative z-10 text-center w-1/2 max-md:w-11/12 text-white overflow-hidden">
+    <div className="relative z-20 text-center w-1/2 max-md:w-11/12 text-white overflow-hidden">
     <h1 className="text-5xl font-bold max-md:text-4xl text-[var(--main-color)] mb-10">وايت قاردن</h1>
     <p className="text-lg leading-relaxed max-md:text-base"> تجربة باقية ولحظات تلامس الحواس تجمع العائلة والأصدقاء والفصول الأربعة بلطف ولذة النكهات وعبق المبخرة والدلة السعودية من أجل أوقات ممتعة تفوق الأماني، ومجموعة جديدة من التجارب والفعاليات الاستثنائية لعشاق الأطباق الشهية.</p>
     </div>
