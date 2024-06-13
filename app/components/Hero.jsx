a3b063f8107baf064f0044343ae804e8ec4a077d
Hazem ali <81050076+hazemali16@users.noreply.github.com>
2024-06-13 12:05:36 +0300

    Update Hero.jsx

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -2,7 +2,7 @@
 
 const Hero = () => {
   return (
-  <div className="h-screen flex items-center justify-center" id="hero">
+  <div className="relative h-screen flex justify-center items-center hero" id="hero">
     <div className="relative z-10 text-center w-1/2 max-md:w-11/12 text-white overflow-hidden">
     <h1 className="text-5xl font-bold max-md:text-4xl text-[var(--main-color)] mb-10">وايت قاردن</h1>
     <p className="text-lg leading-relaxed max-md:text-base"> تجربة باقية ولحظات تلامس الحواس تجمع العائلة والأصدقاء والفصول الأربعة بلطف ولذة النكهات وعبق المبخرة والدلة السعودية من أجل أوقات ممتعة تفوق الأماني، ومجموعة جديدة من التجارب والفعاليات الاستثنائية لعشاق الأطباق الشهية.</p>
