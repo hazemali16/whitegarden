beebc6ef7071b558a63116a51fd17c6ee04facc5
Hazem ali <81050076+hazemali16@users.noreply.github.com>
2024-06-13 16:47:42 +0300

    Update Services.jsx

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import service1 from "../images/DSC07815.jpg"
-import service2 from "../images/DSC07856.jpg"
 import service3 from "../images/DSC07822.jpg"
 import service4 from "../images/services1.jpg"
 import service5 from "../images/services2.jpg"
@@ -14,13 +13,10 @@ import service9 from "../images/services6.jpg"
 const Services = () => {
   return (
     <div className="w-10/12 mx-auto py-40 text-center opacity-0 translate-y-10 duration-500" id="services">
-      <p className="w-1/2 leading-relaxed mx-auto max-lg:w-3/4 max-md:w-full text-xl text-[var(--heading-color)]">يفهم وايت قاردن مدى توقعاتكم ومعايير الجمال التي تليق بكل اللحظات واللقاءات والمناسبات؛ ليقدم لكم تشكيلة مأكولات وقهوة صنعت بحب وتجدد عن كل لقاء لتكتشفوا أروع صور التميز والإبداع في فنون الطهي وعالم النكهات التي تناسب كل الأعمار.</p>
+      <p className="w-1/2 leading-relaxed mx-auto max-lg:w-3/4 max-md:w-full text-xl text-[var(--heading-color)]">يفهم وايت قاردن مدى توقعاتكم ومعايير الجمال التي تليق بكل اللحظات واللقاءات والمناسبات؛ ليقدم لكم تشكيلة مأكولات وقهوة صنعت بحب وتتجدد عن كل لقاء لتكتشفوا أروع صور التميز والإبداع في فنون الطهي وعالم النكهات التي تناسب كل الأعمار.</p>
       <div className="grid mt-20 grid-cols-3 gap-5 max-lg:grid-cols-2 max-md:grid-cols-1">
         <div className="service opacity-0 translate-y-10 duration-500 delay-500">
           <Image src={service1} alt="service1"/>
-        </div>        
-        <div className="service opacity-0 translate-y-10 duration-500 delay-700">
-          <Image src={service2} alt="service2"/>
         </div>        
         <div className="service opacity-0 translate-y-10 duration-500 delay-1000">
           <Image src={service3} alt="service3"/>
@@ -49,3 +45,4 @@ const Services = () => {
 };
 
 export default Services;
+
