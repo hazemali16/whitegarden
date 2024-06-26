f23631c42d9f07d1af6f9bc6101bbb1690beb677
Hazem ali <81050076+hazemali16@users.noreply.github.com>
2024-06-26 14:26:45 +0300

    Update Hero.jsx

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <Image className="w-full h-full object-cover" src={hero} alt="hero" />
     </div>
     <div className="relative z-20 text-center w-1/2 max-md:w-11/12 text-white overflow-hidden">
-    <p className="text-lg leading-relaxed max-md:text-sm"> تجربة باقية ولحظات تلامس الحواس تجمع العائلة والأصدقاء والفصول الأربعة بلطف ولذة النكهات وعبق المبخرة والدلة السعودية من أجل أوقات ممتعة تفوق الأماني، ومجموعة جديدة من التجارب والفعاليات الاستثنائية لعشاق الأطباق الشهية.</p>
+    <h1 className="text-lg leading-relaxed max-md:text-sm"> تجربة باقية ولحظات تلامس الحواس تجمع العائلة والأصدقاء والفصول الأربعة بلطف ولذة النكهات وعبق المبخرة والدلة السعودية من أجل أوقات ممتعة تفوق الأماني، ومجموعة جديدة من التجارب والفعاليات الاستثنائية لعشاق الأطباق الشهية.</h1>
     </div>
   </div>
 );
